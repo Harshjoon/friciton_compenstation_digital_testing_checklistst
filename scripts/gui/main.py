1fbf14927a3b0f4d2a7dd916abbfb38eb68a4d81
Harsh joon <harsh.joon@ssinnovations.org>
2023-09-20 16:47:19 +0530

    new user page added

--- scripts/gui/main.py
+++ scripts/gui/main.py
@@ -3,10 +3,17 @@
 '''
 TODO
 - add login window                   : Pending
+- encryption algorithm for users     : Pending
 - Fixed irregular spaces in document : Pending
+
+- password protect sqlite database   : Pending
+
+- save data in json and connect it
+  to a database                      : Pending 
 '''
 
 import sys
+import os
 from PyQt6.QtGui import QRegularExpressionValidator
 from PyQt6.QtCore import Qt, QSize, QRegularExpression
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel
@@ -27,14 +34,26 @@ from PyQt6.QtWidgets import (
     QPlainTextEdit
 )
 
+import datetime as dt
+
+from make_document import make_document
+from make_meta_data import make_meta_data
+
+
 class Main_window(QMainWindow):
-    def __init__(self):
+    def __init__(self,admin_user, login_details, approver_user):
         super().__init__()
+        self.admin_user         = admin_user
+        self.approver_user      = approver_user
         self.window_height      = 750
         self.window_width       = 800
-        self.init_UI()
+
+        #print(login_details['username'])
+        self.login_details      = login_details
 
         self.meta_data          = {}
+
+        self.init_UI()
 
     def init_UI(self):
         
@@ -94,7 +113,7 @@ drive"                                        : None,
         x_start,y_start = 500,150
         y = y_start
         for key,value in self.checklist_content.items():
-            self.checklist_checkboxes.append(
+            self.checklist_lineedits.append(
                 self.make_lineedit("",x_start,y-5,250,30)
             )
             y += 32
@@ -130,66 +149,162 @@ drive"                                        : None,
         self.end_remark_label    = self.make_label("Remarks",40,600,10)
         self.end_remark_lineedit = self.make_lineedit("",40,620,700,60)
 
-
-
-        # text = "No name found"
-        # self.approved_by_name           = self.make_label(text)
-        # text = "No date found"
-        # self.approved_by_date           = self.make_label(text)
-        # text = "No signature found"
-        # self.approved_by_signature      = self.make_label(text)
-
-
-
-
-
-
+        a = 20
         text = "Assembled by: "
-        self.assembled_by_name_label          = self.make_label(text,40,500,10)
+        self.assembled_by_name_label          = self.make_label(text,40 + a,500,10)
         text = "Date: "
-        self.assembled_by_date_label          = self.make_label(text,300,500,10)
+        self.assembled_by_date_label          = self.make_label(text,300 ,500,10)
         text = "Signature: "
-        self.assembled_by_signature_label     = self.make_label(text,500,500,10)
-        text = "No name found"
-        self.assembled_by_name                = self.make_label(text,140,500,10)
+        self.assembled_by_signature_label     = self.make_label(text,500 ,500,10)
+        text = self.login_details['username']#"No name found"
+        self.assembled_by_name                = self.make_label(text,140 + a,500,10)
         text = "No date found"
-        self.assembled_by_date                = self.make_label(text,350,500,10)
+        self.assembled_by_date                = self.make_label(text,350 ,500,10)
         text = "No signature found"
-        self.assembled_by_signature           = self.make_label(text,580,500,10)
+        self.assembled_by_signature           = self.make_label(text,580 ,500,10)
+        self.assembled_checkbox               = self.make_check_box("",30,502)
 
         text = "Tested by: "
-        self.tested_by_name_label             = self.make_label(text,40,530,10)
+        self.tested_by_name_label             = self.make_label(text,40 + a,530,10)
         text = "Date: "
         self.tested_by_date_label             = self.make_label(text,300,530,10)
         text = "Signature: "
         self.tested_by_signature_label        = self.make_label(text,500,530,10)
         text = "No name found"
-        self.tested_by_name                   = self.make_label(text,140,530,10)
+        self.tested_by_name                   = self.make_label(text,140 + a,530,10)
         text = "No date found"
         self.tested_by_date                   = self.make_label(text,350,530,10)
         text = "No signature found"
         self.tested_by_signature              = self.make_label(text,580,530,10)
+        self.tested_checkbox                  = self.make_check_box("",30,532)
 
         text = "Approved by"
-        self.approved_by_name_label           = self.make_label(text,40,560,10)
+        self.approved_by_name_label           = self.make_label(text,40 + a,560,10)
         text = "Date: "
         self.approved_by_date_label           = self.make_label(text,300,560,10)
         text = "Signature: "
         self.approved_by_signature_label      = self.make_label(text,500,560,10)
         text = "No name found"
-        self.approved_by_name                 = self.make_label(text,140,560,10)
+        self.approved_by_name                 = self.make_label(text,140 + a,560,10)
         text = "No date found"
         self.approved_by_date                 = self.make_label(text,350,560,10)
         text = "No signature found"
         self.approved_by_signature            = self.make_label(text,580,560,10)
-
+        self.approved_checkbox                 = self.make_check_box("",30,562)
 
         self.save_data_button                 = self.make_button("Save data",40,700)
-        self.make_document_button             = self.make_button("Make document",300,700)
+        self.show_document_button             = self.make_button("Show document",200,700)
+        self.make_document_button             = self.make_button("Make document",400,700)
         self.request_approval_button          = self.make_button("Request approval",600,700)
+
+        self.connect_signals_and_slots()
 
         return None
     
+    def connect_signals_and_slots(
+            self
+    ):
+        
+        #self.actuator_find_button
+
+        #self.save_data_button
+        self.make_document_button.clicked.connect(self.on_make_document_clicked)
+        #self.request_approval_button 
+        self.show_document_button.clicked.connect(lambda:  self.show_document(document_path = "../../documents/checklist_report.docx"))
+
+        self.assembled_checkbox.stateChanged.connect(self.on_assemble_state_change)
+        self.tested_checkbox.stateChanged.connect(self.on_tested_state_change)
+        self.approved_checkbox.stateChanged.connect(self.on_approved_state_change)
+
+        return None
+    
+    def on_assemble_state_change(self):
+        
+        if self.assembled_checkbox.isChecked():
+
+            if self.admin_user or self.approver_user:
+                # show message
+                choice = QMessageBox.information(
+                    None,
+                    "Warning",
+                    "Are you sure that you want to make this change.",
+                    QMessageBox.StandardButton.Yes,
+                    QMessageBox.StandardButton.No,
+                )
+                if choice == QMessageBox.StandardButton.No:
+                    self.assembled_checkbox.setChecked(False)
+                    return None
+                elif choice == QMessageBox.StandardButton.Yes:
+                    pass
+
+            date_format = "%d-%m-%Y" 
+            self.assembled_by_name.setText(self.login_details['username'])
+            self.assembled_by_date.setText(dt.datetime.now().strftime(format=date_format))
+            
+            #self.assembled_by_signature
+
+        elif not self.assembled_checkbox.isChecked():
+            self.assembled_by_name.setText("No name found")
+            self.assembled_by_date.setText("No date found")
+            self.assembled_by_name.adjustSize()
+            self.assembled_by_date.adjustSize()
+
+        return None
+    
+    def on_tested_state_change(self):
+        
+        if self.tested_checkbox.isChecked():
+
+            if self.admin_user or self.approver_user:
+                # show message
+                choice = QMessageBox.information(
+                    None,
+                    "Warning",
+                    "Are you sure that you want to make this change.",
+                    QMessageBox.StandardButton.Yes,
+                    QMessageBox.StandardButton.No,
+                )
+                if choice == QMessageBox.StandardButton.No:
+                    self.tested_checkbox.setChecked(False)
+                    return None
+                elif choice == QMessageBox.StandardButton.Yes:
+                    pass
+
+            date_format = "%d-%m-%Y"
+            self.tested_by_name.setText(self.login_details['username'])
+            self.tested_by_date.setText(dt.datetime.now().strftime(format=date_format))
+            #self.tested_by_signature
+
+        elif not self.tested_checkbox.isChecked():
+            self.tested_by_name.setText("No name found")
+            self.tested_by_date.setText("No date found")
+            self.tested_by_name.adjustSize()
+            self.tested_by_date.adjustSize()
+
+        return None
+    
+    def on_approved_state_change(self):
+        if not (self.admin_user or self.approver_user):
+            choice = QMessageBox.critical(
+                None,
+                "Warning",
+                "Only admin user or approver user can approve checlist.",
+                QMessageBox.StandardButton.Ok
+            )
+            self.approved_checkbox.setChecked(False)
+            if choice == QMessageBox.StandardButton.Ok:
+                self.approved_checkbox.setChecked(False)
+                return None
+
+        else:
+            if self.approved_checkbox.isChecked():
+                date_format = "%d-%m-%Y"
+                self.approved_by_name.setText(self.login_details['username'])
+                self.approved_by_date.setText(dt.datetime.now().strftime(format=date_format))
+                #self.approved_by_signature
+
+        return None
+
     def make_label(
             self,
             text        = "",
@@ -261,10 +376,51 @@ drive"                                        : None,
         button.adjustSize()
         return button
 
+    def on_save_clicked(self):
+        """
+            Description : Save data in a database
+        """
+        return None
+    
+    def on_make_document_clicked(self):
+        """
+            Description : make word document and pdf.
+        """
+
+        meta_data = make_meta_data(self,default=False)
+
+        make_document(
+            meta_data=meta_data,
+            save_pdf=True
+        )
+
+        print(meta_data)
+
+        return None
+
+    def show_document(
+            self,
+            document_path = "../../documents/checklist_report.docx"
+            ):
+        os.system('start {}'.format(document_path))
+        return None
+
+    def make_document_number(
+            self
+    ):
+        
+        
+
+        return None
 
 def main():
     app     = QApplication(sys.argv)
-    window  = Main_window()
+    
+    window  = Main_window(
+        admin_user=False,
+        login_details={"username":"admin"},
+        approver_user=False
+    )
     window.show()
     app.exec()
 
---
+++ scripts/gui/new_user_page.py
@@ -0,0 +1,212 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+from PyQt6.QtGui import QRegularExpressionValidator
+from PyQt6.QtCore import Qt, QSize, QRegularExpression
+from PyQt6.QtWidgets import (
+    QCheckBox,
+    QPushButton,
+    QFileDialog,
+    QLineEdit,
+    QTableWidget,
+    QFormLayout,
+    QTabWidget,
+    QVBoxLayout,
+    QMainWindow,
+    QDialog,
+    QGridLayout,
+    QDialogButtonBox,
+    QMessageBox,
+    QSizePolicy
+)
+
+import sqlite3
+from sqlite3 import Error
+
+class NewUserPage(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        print("new user page initialized")
+
+        self.setWindowTitle('Login')
+        # set window icon
+        # self.setWindowIcon(QIcon(''))
+        self.database_filepath = "../../databases/main.db"
+
+        self.window_width, self.window_height = 600,200
+        self.setFixedSize( self.window_width, self.window_height )
+
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        self.labels = {}
+        self.lineEdits = {}
+
+        self.labels['New Username'] = QLabel('New Username')
+        self.labels['Password'] = QLabel('Password')
+        self.labels['Confirm Password'] = QLabel('Confirm Password')
+        self.labels['New Username'].setSizePolicy(QSizePolicy.Policy.Fixed,QSizePolicy.Policy.Fixed)
+        self.labels['Password'].setSizePolicy(QSizePolicy.Policy.Fixed,QSizePolicy.Policy.Fixed)
+        self.labels['Confirm Password'].setSizePolicy(QSizePolicy.Policy.Fixed,QSizePolicy.Policy.Fixed)
+
+        self.lineEdits['New Username']     = QLineEdit()
+        self.lineEdits['Password']     = QLineEdit()
+        self.lineEdits['Password'].setEchoMode(QLineEdit.EchoMode.Password)
+        self.lineEdits['Confirm Password']     = QLineEdit()
+        self.lineEdits['Confirm Password'].setEchoMode(QLineEdit.EchoMode.Password)
+
+        layout.addWidget(self.labels['New Username'],                    0,0,1,1)
+        layout.addWidget(self.lineEdits['New Username'],            0,1,1,3)
+        layout.addWidget(self.labels['Password'],                    1,0,1,1)
+        layout.addWidget(self.lineEdits['Password'],            1,1,1,3)
+        layout.addWidget(self.labels['Confirm Password'],            2,0,1,1)
+        layout.addWidget(self.lineEdits['Confirm Password'],    2,1,1,3)
+
+
+        self.approver_user_checkbox            = QCheckBox("Approver User", self)
+        self.approver_user_checkbox.move(30,90)
+
+        # login_button = QPushButton('&Remove User')#, clicked=self.checkCredential)
+        # layout.addWidget(login_button,                          3,3,1,1)
+
+        add_users_button = QPushButton('&Add Users', clicked=self.add_user)
+        layout.addWidget(add_users_button,                      3,3,1,1)
+
+        self.status = QLabel('')
+        self.status.setStyleSheet('font-size: 25px; color: red;')
+        layout.addWidget(self.status,                           4,0,1,3)
+
+    def initUI(self):
+        return None
+    
+    def add_user(self):
+
+        new_username        = self.lineEdits['New Username'].text()
+        password            = self.lineEdits['Password'].text()
+        confirm_password    = self.lineEdits['Confirm Password'].text()
+
+        if new_username == "":
+            # show warning message
+            choice = QMessageBox.critical(
+                None,
+                "Warning",
+                "New username cannot be empty",
+                QMessageBox.StandardButton.Ok
+            )
+            if choice == QMessageBox.StandardButton.Ok:
+                return None
+            
+        if password == "":
+            # show warning message
+            choice = QMessageBox.critical(
+                None,
+                "Warning",
+                "Password cannot be empty",
+                QMessageBox.StandardButton.Ok
+            )
+            if choice == QMessageBox.StandardButton.Ok:
+                return None
+
+
+        if password != confirm_password:
+            # show warning message
+            choice = QMessageBox.critical(
+                None,
+                "Warning",
+                "Confirm password does not match.",
+                QMessageBox.StandardButton.Ok
+            )
+            if choice == QMessageBox.StandardButton.Ok:
+                return None
+
+        # serch user in database
+        db      = sqlite3.connect(self.database_filepath)
+        cursor  = db.cursor()
+        cursor.execute("SELECT * FROM users WHERE username=?",[new_username])
+        cursor_output = cursor.fetchall()
+        if len(cursor_output) != 0:
+            choice = QMessageBox.critical(
+                None,
+                "Warning",
+                "Username already exists",
+                QMessageBox.StandardButton.Ok
+            )
+            if choice == QMessageBox.StandardButton.Ok:
+                return None
+        
+        cursor.execute("SELECT * FROM admins WHERE adminname=?",[new_username])
+        cursor_output = cursor.fetchall()
+        if len(cursor_output) != 0:
+            choice = QMessageBox.critical(
+                None,
+                "Warning",
+                "Username already exists",
+                QMessageBox.StandardButton.Ok
+            )
+            if choice == QMessageBox.StandardButton.Ok:
+                return None
+            
+        cursor.execute("SELECT * FROM approvers WHERE username=?",[new_username])
+        cursor_output = cursor.fetchall()
+        if len(cursor_output) != 0:
+            choice = QMessageBox.critical(
+                None,
+                "Warning",
+                "Username already exists",
+                QMessageBox.StandardButton.Ok
+            )
+            if choice == QMessageBox.StandardButton.Ok:
+                return None
+        
+        choice = QMessageBox.information(
+            None,
+            "Message",
+            "User added successfully",
+            QMessageBox.StandardButton.Ok
+        )
+
+        try:
+            # add user to database
+            if self.approver_user_checkbox.isChecked():
+                cursor.execute( 'INSERT INTO "approvers" VALUES(\'{0}\',\'{1}\')'.format(new_username, password))
+            else: 
+                cursor.execute( 'INSERT INTO "users" VALUES(\'{0}\',\'{1}\')'.format(new_username, password))
+
+            db.commit()
+            db.close()
+        except sqlite3.Error as er:
+            choice = QMessageBox.critical(
+                None,
+                "Warning",
+                "User cannot be added, please contact developers.",
+                QMessageBox.StandardButton.Ok
+            )
+            if choice == QMessageBox.StandardButton.Ok:
+                return None
+
+        db.close()
+        
+        self.close()
+
+        return None
+    
+    def remove_user(self):
+        return None
+
+def main():
+    print("This is the main function.")
+    app = QApplication(sys.argv)
+    # set app stylesheet
+    # app.setStyleSheet()
+    loginWindow = NewUserPage()
+    loginWindow.show()
+
+    try:
+        sys.exit(app.exec())
+    except SystemExit:
+        print('Closing Window ...')
+
+if __name__ == '__main__':
+    main()
+        
